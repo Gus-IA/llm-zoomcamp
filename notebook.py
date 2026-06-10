70059641f61b273cbf828bc7b6d0f535fe89fc5a
Gus-IA <garciagus1992@gmail.com>
2026-06-10 10:18:58 +0200

    Search

--- notebook.py
+++ notebook.py
@@ -71,3 +71,32 @@ for course in courses_raw:
     documents.extend(course_data)
 
 print(len(documents))
+
+
+from minsearch import Index
+
+index = Index(text_fields=["question", "section", "answer"], keyword_fields=["course"])
+
+index.fit(documents)
+
+search_results = index.search(
+    question,
+    boost_dict={"question": 2.0},
+    filter_dict={"course": "llm-zoomcamp"},
+    num_results=5,
+)
+
+print(search_results)
+
+
+def search(question, course="llm-zoomcamp"):
+    boost_dict = {"question": 2.0, "section": 0.5}
+    filter_dict = {"course": course}
+
+    return index.search(
+        question, boost_dict=boost_dict, filter_dict=filter_dict, num_results=5
+    )
+
+
+search_results = search(question)
+print(search_results)
