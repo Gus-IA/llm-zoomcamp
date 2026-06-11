1dfa0a939dca558e9185085859242f87cea285a1
Gus-IA <garciagus1992@gmail.com>
2026-06-11 09:29:23 +0200

    Prompt

--- notebook.py
+++ notebook.py
@@ -100,3 +100,46 @@ def search(question, course="llm-zoomcamp"):
 
 search_results = search(question)
 print(search_results)
+
+INSTRUCTIONS = """
+Your task is to answer questions from the course participants
+based on the provided context.
+
+Use the context to find relevant information and provide accurate
+answers. If the answer is not found in the context,
+respond with "I don't know."
+"""
+
+USER_PROMPT_TEMPLATE = """
+Question:
+{question}
+
+Context:
+{context}
+"""
+
+
+def build_context(search_results):
+    lines = []
+
+    for doc in search_results:
+        lines.append(doc["section"])
+        lines.append("Q: " + doc["question"])
+        lines.append("A: " + doc["answer"])
+        lines.append("")
+
+    return "\n".join(lines).strip()
+
+
+context = build_context(search_results)
+print(context)
+
+
+def build_prompt(question, search_results):
+    context = build_context(search_results)
+    prompt = USER_PROMPT_TEMPLATE.format(question=question, context=context)
+    return prompt.strip()
+
+
+prompt = build_prompt(question, search_results)
+print(prompt)
